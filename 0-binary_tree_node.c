af4c0df68fe027755a2e49d9f2a2c0d76ba48678
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-10-03 04:58:51 -0700

    a function that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,13 +14,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return NULL;
 	}
-
-
 	new_node->value = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
-
 	return new_node;
 }
